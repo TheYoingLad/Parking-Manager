3939bcc180c4d2fc869aa3636d2384b0531ef172
TheYoingLad <golyhagergo@gmail.com>
2026-08-05 18:09:17 +0200

    added basic reservation logic

--- src/data/Spot.java
+++ src/data/Spot.java
@@ -10,4 +10,16 @@ public class Spot {
     public Spot(Type type) {
         this.type = type;
     }
+
+    public boolean reserve(Reservation incomingReservation){
+        // check whether type matches if the spot is exclusive
+        if (type.isExcusive && incomingReservation.type() != type) return false;
+
+        // check availability
+        if (reservations.stream().anyMatch(registeredReservation -> registeredReservation.interval().overlaps(incomingReservation.interval()))) return false;
+
+        // make reservation
+        reservations.add(incomingReservation);
+        return true;
+    }
 }
